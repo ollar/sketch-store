e7338fe9c259a331eeeb26acfb2c82a7db3e54af
Oleg Larkin <olegollar@gmail.com>
2017-06-19 00:04:52 +0300

    models adn routes added

--- app/router.js
+++ app/router.js
@@ -9,11 +9,21 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('category', function() {
     this.route('index');
+    this.route('details', { path: '/:category_id' });
+    this.route('update', { path: '/update/:category_id' });
   });
   this.route('product', function() {
     this.route('index');
   });
+
   this.route('contacts');
+
+  this.route('page', function() {
+    this.route('details', { path: '/:page_id' });
+    this.route('update', { path: '/update/:page_id'});
+  });
+
+  this.route('not-found', { path: '/*path' });
 });
 
 export default Router;
---
+++ tests/unit/routes/category/update-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:category/update', 'Unit | Route | category/update', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
