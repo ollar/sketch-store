bfb16f0480afc8c650adf2576391103cd3f8197b
Ларькин Олег Иванович <o.larkin@bssys.com>
2017-06-21 16:54:57 +0300

    translations addded

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -8,8 +8,30 @@ export default {
     title: 'List of all pages here'
   },
 
+  categories: {
+    title: 'List of all categories here'
+  },
+
+  forms: {
+    title: {
+      label: 'Title',
+      placeholder: 'Title',
+    },
+  },
+
   buttons: {
     submit: 'Submit',
+    add_block: 'Add block',
+    remove_block: 'Remove block',
+    remove_page: 'Remove page',
+    remove_category: 'Remove category',
+  },
+
+  links: {
+    create_category: 'Create category',
+    create_page: 'Create page',
+    page_edit: 'Edit page',
+    category_edit: 'Edit category',
   },
 
   messages: {
