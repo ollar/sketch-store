cbb5dd0a9d46aa458129fcf85867b4180b81aed8
Oleg Larkin <olegollar@gmail.com>
2017-06-23 01:01:11 +0300

    products dev 2

--- app/controllers/product-create.js
+++ app/controllers/product-create.js
@@ -3,4 +3,26 @@ import BlockManagerMixin from '../mixins/block-manage';
 import ImageManageMixin from '../mixins/image-manage';
 
 export default Ember.Controller.extend(BlockManagerMixin, ImageManageMixin('product'), {
+  blocks: Ember.computed.alias('model.blocks'),
+  images: Ember.computed.alias('model.images'),
+
+  actions: {
+    handleSubmit() {
+      const product = this.get('model');
+      product.set('dateCreated', new Date());
+      product.set('modified', new Date());
+
+      this.get('blocks').forEach((block) => {
+        block.save();
+      });
+
+      product.save().then(() => {
+        this.send('notify', {
+          type: 'success',
+          text: this.get('i18n').t('messages.product_create_success'),
+        });
+        this.transitionToRoute('product', product);
+      });
+    }
+  },
 });
--- app/models/product.js
+++ app/models/product.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  title: DS.attr('string'),
+  blocks: DS.hasMany('block'),
+  images: DS.hasMany('image'),
+  price: DS.attr('number'),
+  category: DS.belongsTo('category'),
+  dateCreated: DS.attr('date'),
+  modified: DS.attr('date'),
 });
