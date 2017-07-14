8daeb6a093f34034a29bd2a20b04a00218948ee0
Ларькин Олег Иванович <o.larkin@bssys.com>
2017-07-14 15:43:21 +0300

    dev

--- app/transitions.js
+++ app/transitions.js
@@ -1,31 +1,31 @@
 export default function() {
   this.transition(
-    this.fromRoute('pages'), this.fromRoute('categories'), this.fromRoute('products'),
-    // this.fromRoute('pages'),
-    this.toRoute('page'), this.toRoute('category'), this.toRoute('product'),
-    // this.toRoute('page'),
+    this.fromRoute(['pages', 'categories', 'products']),
+    this.toRoute(['page', 'category', 'product']),
     this.use('toLeft'),
     this.reverse('toRight')
   );
 
-  // this.transition(
-  //   this.fromRoute('categories'),
-  //   this.toRoute('category'),
-  //   this.use('toLeft'),
-  //   this.reverse('toRight')
-  // );
-
-  // this.transition(
-  //   this.fromRoute('products'),
-  //   this.toRoute('product'),
-  //   this.use('toLeft'),
-  //   this.reverse('toRight')
-  // );
+  this.transition(
+    this.withinRoute(['index', 'pages', 'categories', 'products']),
+    this.use('wait', 100, { then: 'fade' })
+  );
 
   this.transition(
-    this.fromRoute('page.index'),
-    this.toRoute('page.edit'),
+    this.toRoute(['category.edit', 'product.edit', 'page.edit']),
     this.use('crossFade'),
     this.reverse('crossFade')
+  );
+
+  this.transition(
+    this.hasClass('liquid-cart-content-wrapper'),
+    this.toValue(true),
+    this.use('toDown'),
+    this.reverse('toUp')
+  );
+
+  this.transition(
+    this.childOf('.items-number'),
+    this.use('fade')
   );
 }
