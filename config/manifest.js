e42aec499628933314cd9c8a5f30ad54add0ae16
Oleg Larkin <olegollar@gmail.com>
2017-07-19 23:24:14 +0300

    conflicts resolve

--- config/manifest.js
+++ config/manifest.js
@@ -4,7 +4,6 @@
 module.exports = function(/* environment, appConfig */) {
   // See https://github.com/san650/ember-web-app#documentation for a list of
   // supported properties
-
   return {
     name: "sketch-shop",
     short_name: "sketch-shop",
